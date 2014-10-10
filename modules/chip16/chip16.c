101db86ddcb7adb11a6b0b875b879d0f763a7210
Roman Anufriev <mrspeedstalker@gmail.com>
2014-10-10 23:56:18 +0400

    Fixed issues connected with rebasing my MULI branch

--- modules/chip16/chip16.c
+++ modules/chip16/chip16.c
@@ -315,8 +315,6 @@ chip16_string_decode(chip16_t *core, uint32 instr)
 void
 chip16_execute(chip16_t *core, uint32 instr)
 {
-        uint32 res = 0;
-
         uint16 opcode = INSTR_OP(instr);
 
 	uint8 X = INSTR_DST_REG(instr);
@@ -328,7 +326,7 @@ chip16_execute(chip16_t *core, uint32 instr)
 
         uint16 HHLL = INSTR_HHLL(instr);
 
-        int tmp = 0;
+        int res = 0;
 
         switch (opcode) {
 
@@ -340,39 +338,52 @@ chip16_execute(chip16_t *core, uint32 instr)
 
         case Instr_Op_Div_XYZ:
 		if(core->chip16_reg[Y] != 0) {
-			core->chip16_reg[Z] = res = core->chip16_reg[X] / core->chip16_reg[Y];
-			if (res == 0) core->flags.map.Z = 1;
-			else 	      core->flags.map.Z = 0;
+			
+                        core->chip16_reg[Z] = res = core->chip16_reg[X] / core->chip16_reg[Y];
+			
+                        if (res == 0)
+                                SET_ZERO(core->flags);
+			else
+                                CLR_ZERO(core->flags);
 
-			if ((res & (1 << 15)) != 0) core->flags.map.N = 1;
-			else 		 	    core->flags.map.N = 0;
+			if ((res & (1 << 15)) != 0)
+                                SET_NEG(core->flags);
+			else
+                                CLR_NEG(core->flags);
 
-			if (res > 0) {core->flags.map.N = 0; core->flags.map.N = 0;}
+			if (res > 0) {
+                                CLR_NEG (core->flags);
+                                CLR_ZERO(core->flags);
+                        }
 
-			if ((core->chip16_reg[X] % core->chip16_reg[Y]) != 0) core->flags.map.C = 1;
+			if ((core->chip16_reg[X] % core->chip16_reg[Y]) != 0)
+                                SET_CARRY(core->flags);
 		}
-		else SIM_LOG_INFO(1, core->obj, 0, "Dividing by zero!\n");
+		else
+                        SIM_LOG_INFO(1, core->obj, 0, "Dividing by zero!\n");
 
-		chip16_increment_cycles(core, 1);
-                chip16_increment_steps(core, 1);
+		chip16_increment_cycles (core, 1);
+                chip16_increment_steps  (core, 1);
                 INCREMENT_PC(core);
+
+                break;
 
         case Instr_Op_MULI:
 
-                tmp = core->chip16_reg[X] * HHLL;
+                res = core->chip16_reg[X] * HHLL;
                 core->chip16_reg[X] *= HHLL;
                 
-                if (tmp > 0xffff)
+                if (res > 0xffff)
                         SET_CARRY(core->flags);
                 else
                         CLR_CARRY(core->flags);
                 
-                if (tmp == 0)
+                if (res == 0)
                         SET_ZERO(core->flags);
                 else
                         CLR_ZERO(core->flags);
 
-                if ( BIT_15(tmp) == 1 )
+                if ( BIT_15(res) == 1 )
                         SET_NEG(core->flags);
                 else
                         CLR_NEG(core->flags);
@@ -488,65 +499,6 @@ set_idle_cycles(void *arg, conf_object_t *obj,
 {
         chip16_t *core = conf_to_chip16(obj);
         core->idle_cycles = SIM_attr_integer(*val);
-        return Sim_Set_Ok;
-}
-
-/*
- * gprs attribute functions
- */
-static attr_value_t
-get_gprs(void *arg, conf_object_t *obj, attr_value_t *idx)
-{
-        chip16_t *cpu = conf_to_chip16(obj);
-        attr_value_t res = SIM_alloc_attr_list(16);
-        for (int i = 0; i < 16; i++) {
-                SIM_attr_list_set_item(&res, i,
-                        SIM_make_attr_uint64(cpu->chip16_reg[i]));
-        }
-        return res;
-}
-
-static set_error_t
-set_gprs(void *arg, conf_object_t *obj,
-                attr_value_t *val, attr_value_t *idx)
-{
-        chip16_t *cpu = conf_to_chip16(obj);
-        for (int i = 0; i < 16; i++) {
-                cpu->chip16_reg[i] = SIM_attr_integer(
-                        SIM_attr_list_item(*val, i));
-        }
-
-        return Sim_Set_Ok;
-}
-
-/*
- * flags attribute functions
- */
-static attr_value_t
-get_flags(void *arg, conf_object_t *obj, attr_value_t *idx)
-{
-        chip16_t *cpu = conf_to_chip16(obj);
-        attr_value_t res = SIM_alloc_attr_list(4);
-
-        SIM_attr_list_set_item(&res, 0, SIM_make_attr_uint64(cpu->flags.map.C));
-        SIM_attr_list_set_item(&res, 1, SIM_make_attr_uint64(cpu->flags.map.Z));
-        SIM_attr_list_set_item(&res, 2, SIM_make_attr_uint64(cpu->flags.map.O));
-        SIM_attr_list_set_item(&res, 3, SIM_make_attr_uint64(cpu->flags.map.N));
-
-        return res;
-}
-
-static set_error_t
-set_flags(void *arg, conf_object_t *obj,
-                attr_value_t *val, attr_value_t *idx)
-{
-        chip16_t *cpu = conf_to_chip16(obj);
-
-        cpu->flags.map.C = SIM_attr_integer(SIM_attr_list_item(*val, 0));
-        cpu->flags.map.Z = SIM_attr_integer(SIM_attr_list_item(*val, 1));
-        cpu->flags.map.O = SIM_attr_integer(SIM_attr_list_item(*val, 2));
-        cpu->flags.map.N = SIM_attr_integer(SIM_attr_list_item(*val, 3));
-
         return Sim_Set_Ok;
 }
 
@@ -1051,22 +1003,6 @@ cr_register_attributes(conf_class_t *cr_class)
                 Sim_Attr_Optional,
                 "i", NULL,
                 "Number of idle cycles.");
-
-        SIM_register_typed_attribute(
-                cr_class, "gprs",
-                get_gprs, NULL,
-                set_gprs, NULL,
-                Sim_Attr_Optional,
-                "[i*]", NULL,
-                "General purpose registers.");
-
-        SIM_register_typed_attribute(
-                cr_class, "flags",
-                get_flags, NULL,
-                set_flags, NULL,
-                Sim_Attr_Optional,
-                "[i*]", NULL,
-                "Flags.");
 }
 
 /* access_type is Sim_Access_Read, Sim_Access_Write, Sim_Access_Execute */
