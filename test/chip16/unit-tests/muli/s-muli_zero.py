c60f15d06d68b96e13b7262c503dce0079bc784b
Grigory Rechistov <grigory.rechistov@phystech.edu>
2014-10-13 21:12:21 +0400

    Revert "Merge branch 'master' of https://github.com/yulyugin/ilab-simics"
    This reverts commit ff65f09a4710c70c51886bfe0561853e2ce071e3, reversing
    changes made to a40dc62d63a6ffc74f1167267d2677e16ca7941c.

--- test/chip16/unit-tests/muli/s-muli_zero.py
+++ test/chip16/unit-tests/muli/s-muli_zero.py
@@ -7,7 +7,7 @@ cli.run_command("run-python-file %s/targets/chip16/machine.py" % conf.sim.worksp
 def test_muli_availability(cpu):
         paddr = 0
         cpu.pc = paddr
-
+        
         cpu.flags = 0
 
         cpu.gprs[7] = 0xab
@@ -16,18 +16,18 @@ def test_muli_availability(cpu):
         # MULI
         simics.SIM_write_phys_memory(cpu, paddr, 0x90070000, 4)
         SIM_continue(1)
-
+        
         # check regs
-        print "MULI_test-2: checking regs..."
+        print "MULI_1: checking regs..."
         stest.expect_equal(cpu.pc, paddr + 4)
         stest.expect_equal(cpu.gprs[7], res)
-        print "MULI_test-2: regs OK."
+        print "MULI_1: regs OK."
 
         # check flags
-        print "MULI_test-2: checking flags..."
+        print "MULI_1: checking flags..."
         stest.expect_equal(cpu.flags, 0b00000100) # ZERO flag
-        print "MULI_test-2: flags OK: ZERO is set."
-
-        print "MULI_test-2: All is OK!"
-
+        print "MULI_1: flags OK: ZERO is set."
+        
+        print "MULI_1: All is OK!"
+        
 test_muli_availability(conf.chip0)
