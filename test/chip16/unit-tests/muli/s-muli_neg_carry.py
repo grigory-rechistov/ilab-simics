821da6eb5fc40f723fa15e35e3f8c0a1f8cae0b5
yulyugin <yulyugin@gmail.com>
2014-10-14 00:21:48 +0400

    Revert "Revert "Merge branch 'master' of https://github.com/yulyugin/ilab-simics""
    This reverts commit c60f15d06d68b96e13b7262c503dce0079bc784b.

--- test/chip16/unit-tests/muli/s-muli_neg_carry.py
+++ test/chip16/unit-tests/muli/s-muli_neg_carry.py
@@ -7,7 +7,7 @@ cli.run_command("run-python-file %s/targets/chip16/machine.py" % conf.sim.worksp
 def test_muli_availability(cpu):
         paddr = 0
         cpu.pc = paddr
-        
+
         cpu.flags = 0
 
         cpu.gprs[7] = 0xab
@@ -17,18 +17,18 @@ def test_muli_availability(cpu):
         # MULI
         simics.SIM_write_phys_memory(cpu, paddr, 0x9007ffff, 4)
         SIM_continue(1)
-        
+
         # check regs
-        print "MULI_1: checking regs..."
+        print "MULI_test-3: checking regs..."
         stest.expect_equal(cpu.pc, paddr + 4)
         stest.expect_equal(cpu.gprs[7], res)
-        print "MULI_1: regs OK."
+        print "MULI_test-3: regs OK."
 
         # check flags
-        print "MULI_1: checking flags..."
+        print "MULI_test-3: checking flags..."
         stest.expect_equal(cpu.flags, 0b10000010) # NEG & CARRY flag
-        print "MULI_1: flags OK: NEG & CARRY is set."
-        
-        print "MULI_1: All is OK!"
-        
+        print "MULI_test-3: flags OK: NEG & CARRY is set."
+
+        print "MULI_test-3: All is OK!"
+
 test_muli_availability(conf.chip0)
