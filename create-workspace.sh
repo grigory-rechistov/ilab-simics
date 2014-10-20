f7a738a9d1ae50ba2c06721d1765a223e1ff645e
Grigory Rechistov <grigory.rechistov@phystech.edu>
2014-10-20 04:56:59 +0400

    Update workspace script to try network path if local is not working

--- create-workspace.sh
+++ create-workspace.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
 # Use this script to populate current directory with Simics workspace contents.
 
+# default local installation path
 if [ -z $SIMICSPATH ]
 then
     SIMICSPATH=/opt/simics/simics-4.6/simics-4.6.100
 fi
 
 WRKSPCSTP=$SIMICSPATH/bin/workspace-setup
+
+if [ ! -f $WRKSPCSTP  ] # default network installation path
+then
+    WRKSPCSTP=/net/katrin/home/simics/simics/simics-4.6/simics-4.6.100/bin/workspace-setup
+fi
+
 if [ ! -f $WRKSPCSTP  ]
 then
     echo "workspace-setup script is not found! Please set SIMICSPATH to point to the Simics Base package directory"
