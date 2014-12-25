3f587bfee775a6b35450816d6f83ce78ddc17d4c
Karim Iskakov <karfly@yandex.ru>
2014-12-26 02:10:57 +0400

    All attributes added. BGC, SPR, FLIP instructions and tests for them added.

---
+++ test/graph16/unit-tests/spr/s-spr-1.py
@@ -0,0 +1,26 @@
+# This test checks DIV instruction
+
+import stest
+
+cli.run_command("run-python-file %s/targets/chip16/machine.py" % conf.sim.workspace)
+
+def test_one_availability(cpu):
+        graph0_addr = 0xfff6
+
+        # SPR 0xAABB
+        simics.SIM_write_phys_memory(cpu, graph0_addr, 0x0301, 2)
+        simics.SIM_write_phys_memory(cpu, graph0_addr, 0xAABB, 2)
+
+        stest.expect_equal(conf.graph0.spriteh, 0xAA)
+        stest.expect_equal(conf.graph0.spritew, 0xBB)
+        print "SPR(1): success"
+
+        # SPR 0xCCDD
+        simics.SIM_write_phys_memory(cpu, graph0_addr, 0x0301, 2)
+        simics.SIM_write_phys_memory(cpu, graph0_addr, 0xCCDD, 2)
+
+        stest.expect_equal(conf.graph0.spriteh, 0xCC)
+        stest.expect_equal(conf.graph0.spritew, 0xDD)
+        print "SPR(2): success"
+
+test_one_availability(conf.chip0)
