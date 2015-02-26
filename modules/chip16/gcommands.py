21f698ed5817d4aad512f6717c2e24aa29fc7044
Roman Proskin <proskin@phystech.edu>
2015-02-27 00:37:28 +0300

    Initial realization of function to load binary files. no test yet.

--- modules/chip16/gcommands.py
+++ modules/chip16/gcommands.py
@@ -8,100 +8,61 @@ from cli import *
 from simics import *
 import cli_impl
 import sim_commands
+import struct
+import binascii
 
 header = dict(
-    version = '',       # Chip16 spec. version
+    version = 0,        # Chip16 specification version
     rom_size = 0,       # size of following header memory in bytes
     pc_start_addr = 0,  # initial pc value
-    checksum = ''       # CRC32 value as a string of hexadecimal number
+    checksum = 0        # CRC32 value as a string of hexadecimal number
     )
 
-def read_header(byte_list):
-    """
-    Reads header info. 
-    Fills the header dictionary with corresponding data 
-
-    Arguments:
-    byte_list -- list structure of bytes
-                (see loading_binary function for details)
-    """
-
-    for i in range(5):
-        byte = byte_list.pop()
-
-    byte = byte_list.pop()
-    header['version'] = '{}.{}'.format(byte[0], byte[1])
-
-    sum_ = 0
-    for i in range(0, 8, 2):
-        byte = byte_list.pop()
-        sum_ = sum_ + int(byte[0], 16) * pow(16, i + 1) \
-                    + int(byte[1], 16) * pow(16, i)
-    if (sum_ > 64 * 1024):
-        raise CliError("Not enough memory")
-
-    header['rom_size'] = sum_
-
-    sum_ = 0
-    for k in range(0, 2):
-        byte = byte_list.pop()
-        sum_ = sum_ + int(byte[0], 16) * pow(16, i + 1) \
-                    + int(byte[1], 16) * pow(16, i)
-    header['pc_start_addr'] = sum_
-
-    for k in range(0, 4):
-        byte = byte_list.pop()
-        header['checksum'] = header['checksum'] + byte
-
 def load_binary(file_path):
-    
+
+    bin_file = open(file_path, 'rb')
+    memory_max_size = 64*1024;
+
     if '.c16' in file_path:
         header_flag = 1
     elif '.bin' in file_path:
         header_flag = 0
-    else: 
-        raise CliError("Incorrect file. Try .c16, .bin extension files")
+    else:
+        bin_file.close()
+        raise CliError("Incorrect format. Try .c16, .bin extension files")
 
-    bin_file = open(file_path, 'rb')
+    if (header_flag):
 
-    byte_list = []
-    line_list = [line for line in bin_file]
+        temp = bin_file.read(5)
+        header['version'] = ord(bin_file.read(1))
+        rom = reduce(lambda rst, d:rst*10+d, struct.unpack("<L", bin_file.read(4)))
+        header['rom_size'] = rom
 
-    # creating list: byte_list - strings of the form 'XX'
-    # X - hexadecimal digit
-    # thus, each element is one byte in hexadecimal representation
+        if (rom > memory_max_size):
+            bin_file.close()
+            raise CliError("Not enough memory")
 
-    for j in line_list:
-        for k in j:
-            byte = hex(ord(k))
-            if len(byte) == 3:
-                byte_line = '0' + byte[2]
-            else:
-                byte_line = byte[2] + byte[3]
-            byte_list.append(byte_line)
+        header['pc_start_addr'] = reduce(lambda rst, d:rst*10+d, struct.unpack("<H", bin_file.read(2)))
+        header['checksum'] = reduce(lambda rst, d:rst*10+d, struct.unpack("<L", bin_file.read(4)))
 
-    # reverse list in order to use pop() method
-    byte_list.reverse()
+        # culculate the checksum and compare it with the
+        # given value in header
+        crc = binascii.crc32(bin_file.read(rom))
+        if (crc & 0xffffffff != header['checksum']):
+            bin_file.close()
+            raise CliError("File has been damaged. CRC checksum failed")
+        # return to the beginning of rom memory
+        bin_file.seek(16, 0)
 
-    if header_flag:
-        # after this we have only rom data
-        read_header(byte_list)
-
-    # create data_list with instructions in integer form
-    # for future write to memory
-    data_list = []
-    data = ''
-    for i in byte_list:
-        for k in range(4):
-            i = byte_list.pop()
-            data = data + i
-        data = int(data, 16)
-        data_list.append(data)
-        data = ''
+    # copy data into device memory
+    offset = 0
+    while (offset < rom - 4):
+        cli.conf.ram0.image.iface.image.set(0x0 + offset, bin_file.read(4))
+        offset = offset + 4
 
     bin_file.close()
 
-new_command("load-binary", load_binary,
+new_command("load_binary", load_binary,
             args = [arg(str_t, "file-path")],
             alias = "ld",
             type = "loading-commands",
@@ -109,4 +70,4 @@ new_command("load-binary", load_binary,
             doc = """
 <b>load-binary</b> is a command to load binary data from Chip16 
 binary files (.c16, .bin) into platform memory. 
-Only argument <i>file_path</i> is the whole path to the binary file""")
+The only argument <i>file_path</i> is the whole path to the binary file""")
