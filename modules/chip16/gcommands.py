2c4b52bf730e9c8a1347dedc2419e50b1851f9d9
Roman Proskin <proskin@phystech.edu>
2015-03-06 15:04:09 +0300

    Gcommand corrected. test added

--- modules/chip16/gcommands.py
+++ modules/chip16/gcommands.py
@@ -10,6 +10,7 @@ import cli_impl
 import sim_commands
 import struct
 import binascii
+import os
 
 header = dict(
     version = 0,        # Chip16 specification version
@@ -19,55 +20,60 @@ header = dict(
     )
 
 def load_binary(file_path):
-
     bin_file = open(file_path, 'rb')
     memory_max_size = 64*1024;
+    bin_file.seek(0, os.SEEK_END)
+    rom = bin_file.tell()
+    bin_file.seek(0)
 
-    if '.c16' in file_path:
-        header_flag = 1
-    elif '.bin' in file_path:
-        header_flag = 0
+    if file_path.split(".").pop() == 'c16':
+        if bin_file.read(4) == 'CH16':
+            bin_file.seek(5)
+            header['version'] = ord(bin_file.read(1))
+            rom = reduce(lambda rst, d:rst*10+d, struct.unpack("<L", bin_file.read(4)))
+            header['rom_size'] = rom
+            header['pc_start_addr'] = reduce(lambda rst, d:rst*10+d, struct.unpack("<H", bin_file.read(2)))
+            header['checksum'] = reduce(lambda rst, d:rst*10+d, struct.unpack("<L", bin_file.read(4)))
+
+            # culculate the checksum and compare it with the
+            # given value in header
+            crc = binascii.crc32(bin_file.read(rom))
+            if (crc & 0xffffffff != header['checksum']):
+                bin_file.close()
+                raise CliError("File has been damaged. CRC checksum failed")
+
+            # return to the beginning of rom memory
+            bin_file.seek(16)
+        else:
+            # RAW chip16 file
+            bin_file.seek(0)
+    elif file_path.split(".").pop() == 'bin':
+        # RAW chip16 file
+        pass
     else:
         bin_file.close()
         raise CliError("Incorrect format. Try .c16, .bin extension files")
 
-    if (header_flag):
-
-        temp = bin_file.read(5)
-        header['version'] = ord(bin_file.read(1))
-        rom = reduce(lambda rst, d:rst*10+d, struct.unpack("<L", bin_file.read(4)))
-        header['rom_size'] = rom
-
-        if (rom > memory_max_size):
-            bin_file.close()
-            raise CliError("Not enough memory")
-
-        header['pc_start_addr'] = reduce(lambda rst, d:rst*10+d, struct.unpack("<H", bin_file.read(2)))
-        header['checksum'] = reduce(lambda rst, d:rst*10+d, struct.unpack("<L", bin_file.read(4)))
-
-        # culculate the checksum and compare it with the
-        # given value in header
-        crc = binascii.crc32(bin_file.read(rom))
-        if (crc & 0xffffffff != header['checksum']):
-            bin_file.close()
-            raise CliError("File has been damaged. CRC checksum failed")
-        # return to the beginning of rom memory
-        bin_file.seek(16, 0)
+    if (rom > memory_max_size):
+        bin_file.close()
+        raise CliError("Not enough memory")
 
     # copy data into device memory
     offset = 0
-    while (offset < rom - 4):
-        cli.conf.ram0.image.iface.image.set(0x0 + offset, bin_file.read(4))
+    cpu = current_processor()
+    while offset < rom:
+#        cli.conf.ram0.image.iface.image.set(0x0 + offset, bin_file.read(4))
+        SIM_write_phys_memory(cpu, offset, reduce(lambda rst, d:rst*10+d, struct.unpack("<L", bin_file.read(4))), 4)
         offset = offset + 4
 
     bin_file.close()
 
-new_command("load_binary", load_binary,
+new_command("chip16-load-binary", load_binary,
             args = [arg(str_t, "file-path")],
             alias = "ld",
             type = "loading-commands",
             short = "writes from Chip16 binary file to memory",
             doc = """
-<b>load-binary</b> is a command to load binary data from Chip16 
-binary files (.c16, .bin) into platform memory. 
-The only argument <i>file_path</i> is the whole path to the binary file""")
+<b>chip16-load-binary</b> is a command to load binary data from Chip16
+binary files (.c16, .bin) into platform memory.
+The only argument <i>file_path</i> is the whole path to the binary file.""")
