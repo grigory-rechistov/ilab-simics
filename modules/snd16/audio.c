19ac9ac0ab128b73fe42144046894cf4411083a2
Roman Anufriev <mrspeedstalker@gmail.com>
2015-03-10 03:26:05 +0300

    Changed coding style of all snd source files

--- modules/snd16/audio.c
+++ modules/snd16/audio.c
@@ -24,96 +24,113 @@
 // IN:  advance_time    - whether to change phase or not
 // OUT: userdata        - phase and waveform-specfic state updated
 // OUT: stream          - samples for waveform
-static void silence(void* userdata, uint8_t* stream, int len_raw, bool advance_time) {
-    int len = len_raw / 2 /* 16 bit */;
-    // silence is just zeroes in all formats
-    assert(userdata);
-    audio_params_t *ap = (audio_params_t*)userdata;
-    memset(stream, 0, len_raw);
-    if (advance_time)
-        ap->phase += len;
-};
+static void silence (void* userdata, uint8_t* stream, int len_raw, bool advance_time)
+        {
+        int len = len_raw / 2 /* 16 bit */;
+        // silence is just zeroes in all formats
+        assert (userdata);
+        audio_params_t *ap = (audio_params_t*)userdata;
+        memset (stream, 0, len_raw);
+        if (advance_time)
+                ap->phase += len;
+        };
 
 // Fill stream with meandre (rectangular waveform)
 // IN:  userdata        - converted to audio_params, current phase and parameters
 // IN:  len_raw         - length in bytes, NOT samples!
 // OUT: userdata        - phase and waveform-specfic state updated
 // OUT: stream          - samples for waveform
-static void meandre(void* userdata, uint8_t* stream, int len_raw) {
-    int len = len_raw / 2; /* 16 bit */
-    assert(userdata);
-    audio_params_t *ap = (audio_params_t*)userdata;
-    assert(ap->limit > ap->phase);
-    int16_t* buf = (int16_t*)stream;
-    int newlen = ap->limit - ap->phase;
-    int silent_samples = 0;
-    if (len > newlen) {
-        // sound should stop earlier than buffer
-        // adjust len, and fill the end of the buffer with silence
-        silent_samples = len - newlen;
-        silence(userdata, stream + newlen*2, silent_samples*2, false);
-        len = newlen;
-    }
-    int16_t sign = (ap->sign != 0)? ap->sign: 1;
-    assert(sign == 1 || sign == -1);
-    for (int i = 0; i < len; i++) {
-        buf[i] = sign * ap->sdl_vol;
-        // Signal sign changes if time for this sample and next sample times
-        // are across a  boundary of period.
-        // Detect boundary as a floor value of current time divied by period
-        if ( (uint64_t)(ap->sample * (ap->phase+1)* ap->signal_freq ) !=
-             (uint64_t)(ap->sample *  ap->phase   * ap->signal_freq ))
-            sign = -sign;
-        ap->phase++;
-    }
-    // account for samples spent for silence
-    ap->phase += silent_samples;
-    // save flag for the next invocation
-    ap->sign = sign;
-}
+static void meandre(void* userdata, uint8_t* stream, int len_raw)
+        {
+        int len = len_raw / 2; /* 16 bit */
+
+        assert (userdata);
+        audio_params_t *ap = (audio_params_t*)userdata;
+
+        assert (ap->limit > ap->phase);
+        int16_t* buf = (int16_t*)stream;
+        int newlen = ap->limit - ap->phase;
+
+        int silent_samples = 0;
+        if (len > newlen)
+                {
+                // sound should stop earlier than buffer
+                // adjust len, and fill the end of the buffer with silence
+                silent_samples = len - newlen;
+                silence (userdata, stream + newlen*2, silent_samples*2, false);
+                len = newlen;
+                }
+
+        int16_t sign = (ap->sign != 0)? ap->sign: 1;
+        assert (sign == 1 || sign == -1);
+        for (int i = 0; i < len; i++)
+                {
+                buf[i] = sign * ap->sdl_vol;
+                // Signal sign changes if time for this sample and next sample times
+                // are across a  boundary of period.
+                // Detect boundary as a floor value of current time divied by period
+                if ( (uint64_t)(ap->sample * (ap->phase+1)* ap->signal_freq ) !=
+                     (uint64_t)(ap->sample *  ap->phase   * ap->signal_freq ))
+                        sign = -sign;
+                ap->phase++;
+                }
+
+        // account for samples spent for silence
+        ap->phase += silent_samples;
+        // save flag for the next invocation
+        ap->sign = sign;
+        }
 
 
 // Dump just generated sound buffer to textual representation for debug purposes.
 // IN:  userdata        - current phase and parameters
 // IN:  stream          - buffer to be dumped
 // IN:  len_raw         - length of buffer in bytes.
-static void dump_waveform(const void* userdata, const uint8_t* stream, int len_raw) {
-    int len = len_raw / 2; /* 16 bit */
-    assert(userdata);
-    const audio_params_t *ap = (audio_params_t*)userdata;
-    const int16_t* buf = (int16_t*)stream;
+static void dump_waveform(const void* userdata, const uint8_t* stream, int len_raw)
+        {
+        int len = len_raw / 2; /* 16 bit */
+        assert (userdata);
+        const audio_params_t *ap = (audio_params_t*)userdata;
+        const int16_t* buf = (int16_t*)stream;
 
-    uint64_t cursample = ap->phase - len;
-    double delta = ap->sample;
-    printf("# Starting from %16lu %6.6g\n", cursample, delta * cursample);
-    for (int i = 0; i < len; i++) {
-        // sample (int) time (double) value (int16_t)
-        printf("%16lu %8.8g %+6d\n", cursample, delta * cursample, buf[i]);
-        cursample++;
-    }
-}
+        uint64_t cursample = ap->phase - len;
+        double delta = ap->sample;
+
+        // TODO: SIM_printf
+        printf ("# Starting from %16lu %6.6g\n", cursample, delta * cursample);
+        for (int i = 0; i < len; i++)
+                {
+                // sample (int) time (double) value (int16_t)
+                printf ("%16lu %8.8g %+6d\n", cursample, delta * cursample, buf[i]);
+                cursample++;
+                }
+        }
 
 // Callback used by SDL when it needs a new portion of waveform.
 // IN:  userdata        - converted to audio_params, current phase and signal parameters
 // IN:  len_raw         - length in bytes, NOT samples!
 // OUT: userdata        - phase and waveform-specfic state updated
 // OUT: stream          - samples for waveform
-void waveform_callback(void* userdata, uint8_t* stream, int len_raw) {
-    assert(userdata);
-    const audio_params_t *ap = (audio_params_t*)userdata;
+void waveform_callback(void* userdata, uint8_t* stream, int len_raw)
+        {
+        assert (userdata);
+        const audio_params_t *ap = (audio_params_t*)userdata;
 
-    if ((ap->signal_freq == 0) || // signal is not oscillating
-         ap->limit <= ap->phase   // we are asked for sound while we've already given all we got
-    ) {
-        silence(userdata, stream, len_raw, true);
-    } else {
-        switch (ap->wave_type) {
-            case Audio_Meandre:
-                meandre(userdata, stream, len_raw);
-                break;
-            default:
-                assert(0 && "unimplemented waveform");
+        if ((ap->signal_freq == 0) || // signal is not oscillating
+             ap->limit <= ap->phase)  // we are asked for sound while we've already given all we got
+                {
+                silence (userdata, stream, len_raw, true);
+                }
+        else
+                {
+                switch (ap->wave_type)
+                        {
+                        case Audio_Meandre:
+                                meandre (userdata, stream, len_raw);
+                                break;
+                        default:
+                                assert (0 && "unimplemented waveform");
+                        }
+                }
+        (void)(SND_DEBUG? dump_waveform (userdata, stream, len_raw): 0);
         }
-    }
-    (void)(SND_DEBUG? dump_waveform(userdata, stream, len_raw): 0);
-}
--- modules/snd16/snd16.c
+++ modules/snd16/snd16.c
@@ -14,7 +14,8 @@
 #include "include/SDL2/SDL.h"
 #include "audio.h"
 
-typedef struct {
+typedef struct
+        {
         /* Simics configuration object */
         conf_object_t obj;
 
@@ -23,17 +24,18 @@ typedef struct {
 
         SDL_AudioDeviceID audiodev; // an audiodevice opened
         audio_params_t audio_params; // parameters to control waveform
-} snd16_t;
+        } snd16_t;
 
 /* Allocate memory for the object. */
 static conf_object_t *
-alloc_object(void *data)
-{
+alloc_object (void *data)
+        {
         snd16_t *snd = MM_ZALLOC(1, snd16_t);
         return &snd->obj;
-}
+        }
 
-lang_void *init_object(conf_object_t *obj, lang_void *data) {
+lang_void *init_object(conf_object_t *obj, lang_void *data)
+        {
         snd16_t *snd = (snd16_t*)obj;
 
         SDL_AudioSpec want;
@@ -51,38 +53,50 @@ lang_void *init_object(conf_object_t *obj, lang_void *data) {
                 SIM_LOG_INFO(1, obj, 0, "Failed to open audio device: %s", SDL_GetError());
 //        SIM_LOG_INFO(1, obj, 0, "Audio device is %s", SDL_GetAudioDeviceName(0, 0)); // incorrect, don't know if possible at all // GGG
         return obj;
-}
+        }
 
-int delete_instance(conf_object_t *obj) {
+int delete_instance (conf_object_t *obj)
+        {
         snd16_t *snd = (snd16_t*)obj;
         if (snd->audiodev)
                 SDL_CloseAudioDevice(snd->audiodev);
         return 1;
-}
+        }
 
 static exception_type_t
-operation(conf_object_t *obj, generic_transaction_t *mop, map_info_t info)
-{
+operation (conf_object_t *obj, generic_transaction_t *mop, map_info_t info)
+        {
         snd16_t *snd = (snd16_t *)obj;
         unsigned offset = (SIM_get_mem_op_physical_address(mop)
                            + info.start - info.base);
 
-        if (SIM_mem_op_is_read(mop)) {
+        if (SIM_mem_op_is_read(mop))
+                {
                 SIM_set_mem_op_value_le(mop, snd->value);
                 SIM_LOG_INFO(1, &snd->obj, 0, "read from offset %d: 0x%x",
                              offset, snd->value);
-        } else {
+                }
+        else
+                {
                 snd->value = SIM_get_mem_op_value_le(mop);
                 SIM_LOG_INFO(1, &snd->obj, 0, "write to offset %d: 0x%x",
                              offset, snd->value);
-        }
+                }
+
         return Sim_PE_No_Exception;
-}
+        }
+
+static attr_value_t
+get_value_attribute (void *arg, conf_object_t *obj, attr_value_t *idx)
+        {
+        snd16_t *snd = (snd16_t *)obj;
+        return SIM_make_attr_uint64(snd->value);
+        }
 
 static set_error_t
-set_value_attribute(void *arg, conf_object_t *obj,
+set_value_attribute (void *arg, conf_object_t *obj,
                     attr_value_t *val, attr_value_t *idx)
-{
+        {
         snd16_t *snd = (snd16_t *)obj;
         snd->value = SIM_attr_integer(*val);
 
@@ -92,36 +106,31 @@ set_value_attribute(void *arg, conf_object_t *obj,
         // For debug purposes, we can write to this attribute to force
         // sound generation.
         if (snd->value == 0)
-            SDL_PauseAudioDevice(snd->audiodev, 1);
-        else {
-            SDL_PauseAudioDevice(snd->audiodev, 1);
-            // prepare meandre parameters
-            snd->audio_params.wave_type = Audio_Meandre;
-            snd->audio_params.sdl_vol = 16000;
-            snd->audio_params.signal_freq = 440;
-            snd->audio_params.period = 1.0d / snd->audio_params.signal_freq;
-            snd->audio_params.phase = 0;
-            snd->audio_params.limit = 7000;
-            snd->audio_params.sign = 1;
+                SDL_PauseAudioDevice(snd->audiodev, 1);
+        else
+                {
+                SDL_PauseAudioDevice(snd->audiodev, 1);
+                // prepare meandre parameters
+                snd->audio_params.wave_type = Audio_Meandre;
+                snd->audio_params.sdl_vol = 16000;
+                snd->audio_params.signal_freq = 440;
+                snd->audio_params.period = 1.0d / snd->audio_params.signal_freq;
+                snd->audio_params.phase = 0;
+                snd->audio_params.limit = 7000;
+                snd->audio_params.sign = 1;
 
-            SDL_PauseAudioDevice(snd->audiodev, 0);
-            SDL_Delay(500);
-            SDL_PauseAudioDevice(snd->audiodev, 1);
-        }
+                SDL_PauseAudioDevice(snd->audiodev, 0);
+                SDL_Delay(500);
+                SDL_PauseAudioDevice(snd->audiodev, 1);
+                }
+
         return Sim_Set_Ok;
-}
-
-static attr_value_t
-get_value_attribute(void *arg, conf_object_t *obj, attr_value_t *idx)
-{
-        snd16_t *snd = (snd16_t *)obj;
-        return SIM_make_attr_uint64(snd->value);
-}
+        }
 
 /* called once when the device module is loaded into Simics */
 void
-init_local(void)
-{
+init_local (void)
+        {
         /* Register the class with callbacks used when creating and deleting
            new instances of the class */
         const class_data_t funcs = {
@@ -131,14 +140,14 @@ init_local(void)
                 .class_desc = "CHIP16 sound device",
                 .description =
                         "Sound device for CHIP16 platform"
-        };
+                };
         conf_class_t *class = SIM_register_class("snd16", &funcs);
 
         /* Register the 'io_memory' interface, which is an example of a generic
            interface that is implemented by all memory mapped devices. */
         static const io_memory_interface_t memory_iface = {
                 .operation = operation
-        };
+                };
         SIM_register_interface(class, IO_MEMORY_INTERFACE, &memory_iface);
 
         /* Register attributes (device specific data) together with functions
@@ -150,10 +159,10 @@ init_local(void)
                 "The <i>value</i> field.");
 
         if (SDL_WasInit(SDL_INIT_AUDIO) == 0) {
-            SIM_printf("SDL audio hasn't been initialized, doing it now\n");
-            SDL_InitSubSystem(SDL_INIT_AUDIO);
-        }
+                SIM_printf("SDL audio hasn't been initialized, doing it now\n");
+                SDL_InitSubSystem(SDL_INIT_AUDIO);
+                }
         // FIXME we also need to call a cleanup, maybe something like this?
         // But see this: https://developer.palm.com/distribution/viewtopic.php?f=82&t=6643
         atexit(SDL_Quit);
-}
+        }
