3ad0736b797a8782144e438fd3704cc6b2651d27
Maria <burkinamaria@github.com>
2015-10-15 20:53:27 +0300

    Changing TABs to spaces

--- test/chip16/unit-tests/or_xyz/s-or-xyz.py
+++ test/chip16/unit-tests/or_xyz/s-or-xyz.py
@@ -5,7 +5,7 @@ import stest
 cli.run_command("run-python-file %s/test/chip16-setup.py" % conf.sim.workspace)
 
 def test_or_availability(cpu):
-	cpu.pc = 0
+        cpu.pc = 0
         cpu.flags = 0
 
         cpu.gprs[1] = 0x1000
@@ -24,7 +24,7 @@ def test_or_availability(cpu):
         cpu.gprs[2] = 0x0
         # OR R1, R2, R3
         chip16_write_phys_memory_BE(cpu, 0, 0x72210A00, 4)
-	SIM_continue(1)
+        SIM_continue(1)
         stest.expect_equal(cpu.pc, 4)
         stest.expect_equal(cpu.flags, 0b00000100)
         print "OR_XYZ: zero success"
@@ -37,7 +37,7 @@ def test_or_availability(cpu):
         # OR R1, R2, R3
         chip16_write_phys_memory_BE(cpu, 0, 0x72210A00, 4)
         SIM_continue(1)
-	stest.expect_equal(cpu.pc, 4)
+        stest.expect_equal(cpu.pc, 4)
         stest.expect_equal(cpu.gprs[0xA], res)
         stest.expect_equal(cpu.flags, 0b10000000)
         print "OR_XYZ: neg success"
